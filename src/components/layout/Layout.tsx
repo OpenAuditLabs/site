04d50f4b29b897fe4c49f5a565fc8ff807e0562d
Tomasz Wójcik <tomas.zwojcik.bbt51@gmail.com>
2025-08-12 22:26:42 -0600

    feat: implement Footer component in Layout for enhanced site structure

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 interface LayoutProps {
   children: ReactNode;
@@ -7,9 +8,10 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-[var(--background)]">
       <Header />
       <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 };
