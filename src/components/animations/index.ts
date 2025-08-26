3bb28059e03a26f1837e82cf7c19e4e1a428ebc7
Joseph Bao <jobaodev@gmail.com>
2025-08-26 02:50:43 -0600

    feat: project theme initialization with shadcn

--- src/components/animations/index.ts
+++
@@ -1,4 +0,0 @@
-// animations exported from here
-export { default as FadeIn } from './FadeIn';
-export { default as StaggerAnimation } from './StaggerAnimation';
-// animations exported from here
--- src/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-// export layout parts from here
-export { default as Header } from "./Header";
-export { default as Layout } from "./Layout";
-export { default as Navbar } from "./Navbar";
-export { default as Sidebar } from "./Sidebar";
