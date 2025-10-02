4bd3b4b3076c08febe4714ccf5983a2cd5e5395a
Nick Verner <59479310+naevern@users.noreply.github.com>
2025-10-02 10:02:08 -0600

    feat: add lint:fix script to package.json and remove unused Button import in HeroCarousel (#172)

--- src/components/landing/hero/HeroCarousel.tsx
+++ src/components/landing/hero/HeroCarousel.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 
 export default function HeroCarousel() {
 	const slides = [
